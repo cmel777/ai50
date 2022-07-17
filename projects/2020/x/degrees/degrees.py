93da3446ac523e71f7a3f90b062880e5d196a1a9
Cephas Melvin <cephasmelvin@Cephass-MacBook-Pro.local>
2022-07-17 12:59:31 +0100

    erroneous working decent draft

--- projects/2020/x/degrees/degrees.py
+++ projects/2020/x/degrees/degrees.py
@@ -1,6 +1,9 @@
 import csv
 import multiprocessing
-import sys
+import sys, threading
+
+sys.setrecursionlimit(10 ** 7)  # max depth of recursion
+threading.stack_size(2 ** 27)  # new thread will get stack of such size
 
 names = {}
 
@@ -12,10 +15,9 @@ movies = {}
 
 count_explored = 0
 
+found_neighbours = []
 
-class ParentPath:
-    def __init__(self, parent_path):
-        self.parent_path = parent_path
+parent_path = set()
 
 
 class Node:
@@ -26,31 +28,51 @@ class Node:
 
 
 node = Node(None, None, None)
-parent_path = ParentPath([])
 
 least_path = 0
 
 
-def print_data():
+def print_people():
     print("People")
     print("*******")
     for item in people.items():
         print(item)
+
+
+def print_movies():
     print("Movies")
     print("*******")
     for item in movies.items():
         print(item)
+
+
+def print_foundneighbours(found):
+    print("Found Neighbours")
+    print("*******")
+    for item in found_neighbours:
+        print(item)
+
+
+def print_names():
     print("Name")
     print("*******")
     for item in names.items():
         print(item)
     print("**********************************************")
-    print("Neighbours")
-    for item in neighbors_for_person("129"):
+
+
+def print_neighbours(source):
+    print("Neighbours for star: " + source)
+    for item in neighbors_for_person(source):
         print(item)
     print("**********************************************")
-    print("**********************************************")
-    print("**********************************************")
+
+
+def print_all(source):
+    print_people()
+    print_movies()
+    print_names()
+    # print_neighbours(source)
 
 
 def load_data(directory):
@@ -100,11 +122,11 @@ def main():
     # Load data from files into memory
     print("Loading data...")
     load_data(directory)
-    print("Data loaded.")
-    print_data()
+    print("Printing data...")
 
     # source = person_id_for_name(input("Name: "))
     source = person_id_for_name("Tom Cruise")
+    print_all(source)
     if source is None:
         sys.exit("Person not found.")
     target = person_id_for_name("Kevin Bacon")
@@ -112,25 +134,32 @@ def main():
     if target is None:
         sys.exit("Person not found.")
 
-    found_neighbours = []
     lock = multiprocessing.Lock()
     # Returns the shortest list of (movie_id, person_id) pairs
-    shortest_path(source, target, node, parent_path, found_neighbours, lock)
+    shortest_path(source, target, node, parent_path, lock, found_neighbours, people, names, movies)
 
     if node is None:
         print("Not connected.")
     else:
-        degrees = len(node.path)
-        print(f"{degrees} degrees of separation.")
-        path = [(None, source)] + node.path
-        for i in range(degrees):
-            person1 = people[path[i][1]]["name"]
-            person2 = people[path[i + 1][1]]["name"]
-            movie = movies[path[i + 1][0]]["title"]
-            print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+        if node.path is not None:
+            degrees = len(node.path)
+            print(f"{degrees} degrees of separation.")
+            path = [(None, source)] + node.path
+            for i in range(degrees):
+                person1 = people[path[i][1]]["name"]
+                person2 = people[path[i + 1][1]]["name"]
+                movie = movies[path[i + 1][0]]["title"]
+                print(f"{i + 1}: {person1} and {person2} starred in {movie}")
 
 
-def shortest_path(source, target, node, parent_path, found_neighbours, lock):
+def found_in_neighbours(source):
+    if str(source) in found_neighbours:
+        return False
+    else:
+        return True
+
+
+def shortest_path(source, target, node, parent_path, lock, found_neighbours, people, names, movies):
     """
     Returns the shortest list of (movie_id, person_id) pairs
     that connect the source to the target.
@@ -145,50 +174,65 @@ def shortest_path(source, target, node, parent_path, found_neighbours, lock):
     # raise Exception("no solution")
 
     # Add neighbors to frontier
-    if source not in found_neighbours:
-        path = []
-        if parent_path.parent_path is not None:
-            path = parent_path.parent_path
-        found_neighbours.append(source)
-        Process_jobs = []
-        for count, item in enumerate(neighbors_for_person(source), start=1):
-            parent_path.parent_path = (item[0], item[1])
-            p = multiprocessing.Process(target=shortest_path,
-                                        args=(item[1], target, node, found_neighbours, parent_path, lock))
-            Process_jobs.append(p)
-            p.start()
-            p.join()
-            if (len(neighbors_for_person(source)) - count) == 0:
-                print("Last neighbour is : " + item[1] + "for star :" + source)
-                return
-            if item[1] == target:
-                path.append((item[0], item[1]))
-                degrees = len(path)
-                if int(node.least_degree) > degrees:
-                    lock.acquire()
-                    node.least_degree = degrees
-                    node.path = path
-                    lock.release()
-                    return
+    if source is not None:
+        if str(source) in found_neighbours:
+            print_foundneighbours(found_neighbours)
+            return
+        else:
+            neighbourliness = neighbors_for_person(str(source), people, movies)
+            found_neighbours.append(source)
+            if parent_path is not None:
+                path = set()
+                path = parent_path
+
+            Process_jobs = []
+            for count, item in enumerate(neighbourliness, start=1):
+                path.add((item[0], item[1]))
+                parent_path.add((item[0], item[1]))
+                print("Source:" + source + " Star:" + item[1] + " Movie:" + item[0])
+                if item[1] is not None:
+                    p = multiprocessing.Process(target=shortest_path,
+                                                args=(item[1], target, node, parent_path, lock, found_neighbours,
+                                                      people, names, movies))
+                    Process_jobs.append(p)
+                    p.start()
+                    p.join()
+                    if item[1] == target:
+                        path.add((item[0], item[1]))
+                        degrees = len(path)
+                        if node.least_degree is not None:
+                            if int(node.least_degree) > degrees:
+                                lock.acquire()
+                                node.least_degree = degrees
+                                node.path = path
+                                lock.release()
+                                return
 
     # TODO
-    raise NotImplementedError
+    sys.exit()
 
 
-def neighbors_for_person(person_id):
+def neighbors_for_person(person_id, people, movies):
     """x
     Returns (movie_id, person_id) pairs for people
     who starred with a given person.
     """
-    movie_ids = people[person_id]["movies"]
-    neighbors = set()
-    for movie_id in movie_ids:
-        for id in movies[movie_id]["stars"]:
-            if id != person_id:
-                neighbors.add((movie_id, id))
-    return neighbors
+    # try:
+    if person_id not in found_neighbours:
+        print("source id inside :" + person_id)
+        movie_ids = people[person_id]["movies"]
+        neighbors = set()
+        for movie_id in movie_ids:
+            for id in movies[movie_id]["stars"]:
+                if id != person_id:
+                    neighbors.add((movie_id, id))
+        return neighbors
+    # except KeyError as err:
+    # print("Person :" + person_id)
+    # print("Person :" + print_neighbours(str(person_id)))
 
 
+"""
 def compute(source, target, node, found_neighbours, parent_path, lock):
     # Add neighbors to frontier
     if source not in found_neighbours:
@@ -217,7 +261,7 @@ def compute(source, target, node, found_neighbours, parent_path, lock):
                     lock.release()
                     return
 
-            """
+            
                 p = multiprocessing.Process(target=compute, args=(item[1], target, node,))
     Process_jobs.append(p)
     p.start()
